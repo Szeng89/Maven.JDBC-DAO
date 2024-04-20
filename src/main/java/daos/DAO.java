accd7d85dcac2130f6873805d4122cdb3892c27c
Szeng89 <shijinz898@gmail.com>
2024-04-20 01:14:05 -0400

    did findById and findAll

--- src/main/java/daos/DAO.java
+++ src/main/java/daos/DAO.java
@@ -1,25 +1,83 @@
 package daos;
 
+import models.Bikes;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
-public class DAO implements daos.DAOInterface {
+public class DAO implements DAOInterface<Bikes> {
+    private final Connection connection1 = DBconnection.getConnection();
     @Override
-    public Object findById(int id) {
+    public Bikes findById(int id) {
+
+        try {
+            Statement stmt = connection1.createStatement();
+            ResultSet rs = stmt.executeQuery("SELECT * FROM user WHERE id=" + id);
+
+            if(rs.next())
+            {
+                return extractUserFromResultSet(rs);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    private Bikes extractUserFromResultSet(ResultSet rs) {
+        try {
+            Bikes myBike = new Bikes();
+
+            myBike.setId(rs.getInt("Id"));
+            myBike.setColor(rs.getNString("Lime Green"));
+            myBike.setMake(rs.getNString("Specialized"));
+            myBike.setModel(rs.getNString("Allez"));
+            myBike.setSize(rs.getInt("Size"));
+            myBike.setYear(rs.getInt("Year"));
+
+            return myBike;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         return null;
     }
 
     @Override
-    public List findAll() {
+    public List<Bikes> findAll() {
+        try {
+            Statement ps = connection1.createStatement();
+            ResultSet rs = ps.executeQuery("Select * From Bikes;");
+            ArrayList<Bikes> bikesList = new ArrayList<>();
+
+
+            if(rs.next())
+            {
+                Bikes bike = extractUserFromResultSet(rs);
+                bikesList.add(bike);
+            }
+            return bikesList;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+
+    }
+
+    @Override
+    public Bikes update(Bikes object) {
         return null;
     }
 
     @Override
-    public Object update(Object dto) {
-        return null;
-    }
-
-    @Override
-    public Object create(Object dto) {
+    public Bikes create(Bikes dto) {
         return null;
     }
 
--- src/main/java/daos/DBconnection.java
+++ src/main/java/daos/DBconnection.java
@@ -1,2 +1,30 @@
-package daos;public class DBconnection {
+package daos;
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBconnection {
+    public static final String URL = "jdbc:mysql://localhost:3306/Bikes";
+    public static final String USER = "shijin";
+    public static final String PASS = "fujingjoeman";
+
+    /**
+     * Get a connection to database
+     * @return Connection object
+     */
+    public static Connection getConnection() {
+        try {
+            DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+            return DriverManager.getConnection(URL, USER, PASS);
+        } catch (SQLException ex) {
+            throw new RuntimeException("Error connecting to the database", ex);
+        }
+    }
+
+
+    public static void main(String[] args) {
+        Connection connection = DBconnection.getConnection();
+    }
 }
